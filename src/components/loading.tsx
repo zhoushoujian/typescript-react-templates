b6c392150a49ea0bcaec0dd42bb0039aacda5cbe
zhoushoujian <qianchengyimeng@qq.com>
2021-03-19 19:00:27 +0800

    feat: add css variety to change skim

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import Style from './index.less'
 
+const colors = ['#000', '#ff0', '#f00', '#f0f', '#0ff', '#00f', '#0f0'];
+
 const Loading = ({ text = "loading" }) => {
+
+  const changeColor = () => {
+    const root = document.documentElement;
+    root.style.setProperty("--primary-color", colors[Math.floor(Math.random() * colors.length)]);
+  };
+
   return (
-		<div className={Style.loading}>{text}</div>
+    <div className={Style.loading} onClick={changeColor}>{text}</div>
   );
 };
 
