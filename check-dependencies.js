9a26f98999b9d9ed11ef23dba1619e7bf9b47174
zhoushoujian <863165094@qq.com>
2021-08-11 11:34:23 +0800

    feat: update @shuyun-ep-team/specified-package-version-check

--- check-dependencies.js
+++ check-dependencies.js
@@ -1,8 +1,10 @@
-const checkDependenceVersion = require('specified-package-version-check');
+const checkDependenceVersion = require('@shuyun-ep-team/specified-package-version-check');
 
 async function func() {
   await checkDependenceVersion({
     dependenceArr: ['logger-for-cannot-duplicate', '@shuyun-ep-team/eslint-config', '@shuyun-ep-team/kylin-ui'],
+    useDepCheck: true,
+    autoFixOutdateDep: true,
   });
 }
 
